382b5f64cb85d7af777db4ddb90cab6ac6169b82
Lucienzan <changwilliam826@gmail.com>
2023-08-06 16:48:01 +0630

    [project/projectone] added db.json and show lists

---
+++ planner/src/components/SingleProject.vue
@@ -0,0 +1,57 @@
+<template>
+
+  <div class="action">
+    <h3 class="article-ttl" @click="isShow = !isShow">{{ project.title }}</h3>
+    <div class="controls">
+        <span class="material-symbols-outlined">
+check
+</span>
+      <span class="material-symbols-outlined">
+edit_note
+</span>
+      <span class="material-symbols-outlined">
+delete
+</span>
+    </div>
+  </div>
+  <div class="d-section" v-if="isShow" >
+    <p>{{ project.detail }}</p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: ["project"],
+  data(){
+    return{
+      isShow: false
+    }
+  },
+ 
+}
+</script>
+
+<style lang="css" scoped>
+
+:is(.article-ttl, .controls :where(span)) {
+  cursor: pointer;
+}
+
+.action {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+  width: 100px;
+}
+
+.d-section {
+  text-align: justify;
+}
+
+</style>
--- planner/src/views/HomeView.vue
+++ planner/src/views/HomeView.vue
@@ -1,16 +1,44 @@
 <template>
   <div class="home">
-
+      <div v-if="projects.length" >
+          <div v-for="project in projects" :key="project.id" class="lists-sec">
+            <SingleProject :project="project"/>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import SingleProject from "@/components/SingleProject.vue";
+
 export default {
   name: 'HomeView',
-  components: {
-    
+  components: {SingleProject},
+  data(){
+    return{
+      projects:[]
+    }
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+        .then(res=>res.json())
+        .then(data => this.projects = data)
+        .catch(err=>console.log(err));
   }
+
 }
 </script>
+<style scoped>
+.lists-sec {
+  background: #f1f1f1;
+  max-width: 650px;
+  margin: 0 auto;
+  padding: 14px 18px;
+  margin-bottom: 20px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
+  border-left: 6px solid #E86A33;
+  border-radius: 5px;
+}
+</style>
